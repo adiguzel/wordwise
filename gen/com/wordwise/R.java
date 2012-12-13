bba03f1693b224c100b9298362b38d3152e11f40
Dragan Mileski <dragan.mileski@gmail.com>
2012-12-13 16:06:08 +0100

    Added picture resources for the hangman game. All are in drawable folder. I wanted to make separate folder in drawable but i got error and from the research i made i saw that this is not possible. So we should keep good naming conventions for the pictures and keep them all in drawable.

--- gen/com/wordwise/R.java
+++ gen/com/wordwise/R.java
@@ -21,10 +21,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int hangman_img00=0x7f020000;
-        public static final int hangman_img1=0x7f020001;
-        public static final int ic_action_search=0x7f020002;
-        public static final int ic_action_settings=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
+        public static final int hangman_img01=0x7f020001;
+        public static final int hangman_img02=0x7f020002;
+        public static final int hangman_img03=0x7f020003;
+        public static final int hangman_img04=0x7f020004;
+        public static final int hangman_img05=0x7f020005;
+        public static final int hangman_img06=0x7f020006;
+        public static final int hangman_img07=0x7f020007;
+        public static final int hangman_img08=0x7f020008;
+        public static final int hangman_img09=0x7f020009;
+        public static final int ic_action_search=0x7f02000a;
+        public static final int ic_action_settings=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
     }
     public static final class id {
         public static final int ScrollView01=0x7f0a0000;
