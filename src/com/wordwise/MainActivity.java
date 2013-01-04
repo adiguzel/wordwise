3d237ceea0b9c1a55c3f6f5944e3218a89ddd462
Ugur Adiguzel <uguradgzl88@gmail.com>
2013-01-04 22:57:55 +0100

    some progress on Memory game

--- src/com/wordwise/MainActivity.java
+++ src/com/wordwise/MainActivity.java
@@ -5,8 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.wordwise.activity.AboutActivity;
-import com.wordwise.activity.MainGameScreen;
 import com.wordwise.activity.MenuActivity;
+import com.wordwise.activity.game.Memory;
 import com.wordwise.activity.setting.ConfigurationWizardStep1;
 import com.wordwise.model.Configuration;
 
@@ -18,9 +18,9 @@ public class MainActivity extends MenuActivity {
        
         if(!Configuration.getInstance(getApplicationContext()).isConfigured())
         	configure();  
-        else{
+        else
         	loadMainScreen();
-        }
+        
         
         /* Setting of the content View should be at the last position in order to receive 
          * configuration screens at first and than avoid having an empty activity if the user 
@@ -44,7 +44,7 @@ public class MainActivity extends MenuActivity {
     }
     
     private void loadMainScreen(){
-    	Intent intent = new Intent(this, MainGameScreen.class);
+    	Intent intent = new Intent(this, Memory.class);
         startActivity(intent);
     }
 }
--- src/com/wordwise/activity/game/Memory.java
+++ src/com/wordwise/activity/game/Memory.java
@@ -1,8 +1,6 @@
 package com.wordwise.activity.game;
 
-import java.util.HashSet;
-import java.util.Set;
-
+import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
@@ -22,15 +20,17 @@ import android.widget.TextView;
 import com.wordwise.R;
 import com.wordwise.R.color;
 import com.wordwise.gameengine.Game;
+import com.wordwise.model.game.Lock;
 
 public class Memory extends Activity implements Game {
-	
+
 	private Button continueButton;
 	private final int IN_PROGRESS = 0;
 	private final int FINISHED = 1;
-	private int gameState = IN_PROGRESS; 
+	private int gameState = IN_PROGRESS;
 	private TextView firstFlipped = null;
-	SharedPreferences SP;
+	private SharedPreferences SP;
+	private Lock flipLock = new Lock();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -62,8 +62,8 @@ public class Memory extends Activity implements Game {
 		continueButton = (Button) findViewById(R.id.continueButton);
 		initMemoryGrid();
 	}
-	
-	private void initMemoryGrid(){
+
+	private void initMemoryGrid() {
 		GridView translationsGrid = (GridView) findViewById(R.id.memoryGrid);
 		translationsGrid.setAdapter(new WordAndTranslationAdapter(this));
 	}
@@ -78,49 +78,116 @@ public class Memory extends Activity implements Game {
 	public void finishGame(View v) {
 
 	}
-	
-	private void flipFaceUp(TextView v){
-		
-		String tag = (String) v.getTag();
-		v.setText(tag);
-		flip(v);
-		if(firstFlipped  == null){
-			firstFlipped = v;
-		}
-		else{ 
-			if(((String)firstFlipped.getTag()).equals((String)v.getTag())){
-				firstFlipped.setBackgroundColor(color.wordwise_main_green_disabled_start);
-				v.setBackgroundColor(color.wordwise_main_green_disabled_start);
+
+	private boolean matches(TextView v1, TextView v2) {
+		return ((String) v1.getTag()).equals((String) v2.getTag());
+	}
+
+	private void flipFaceUp(final TextView v) {
+		Animator.AnimatorListener animListener = new Animator.AnimatorListener() {
+			public void onAnimationCancel(Animator animation) {
+				// TODO Auto-generated method stub
+			}
+
+			public void onAnimationEnd(Animator animation) {
+				// TODO Auto-generated method stub
+				String tag = (String) v.getTag();
+				v.setText(tag);
+				if (firstFlipped == null)
+					firstFlipped = v;
+				flipLock.unlock();
+			}
+
+			public void onAnimationRepeat(Animator animation) {
+				// TODO Auto-generated method stub
+			}
+
+			public void onAnimationStart(Animator animation) {
+				// TODO Auto-generated method stub
 				try {
-					Thread.sleep(1500);
+					flipLock.lock();
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				firstFlipped.setVisibility(View.INVISIBLE);
-				v.setVisibility(View.INVISIBLE);
-				
 			}
-			else{
+		};
+		
+
+		flip(v, animListener);
+
+		Log.v("Lock","is " + flipLock.isLocked() );
+		try {
+			flipLock.lock();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Log.v("Alt LOCk","is " + flipLock.isLocked() );
+		if (firstFlipped != null) {
+			if (matches(firstFlipped, v)) {
+				firstFlipped
+						.setBackgroundColor(color.wordwise_main_green_disabled_end);
+				firstFlipped.invalidate();
+				v.setBackgroundColor(color.wordwise_main_green_disabled_end);
+				v.invalidate();
+				 try { Thread.sleep(1000); } catch
+				 (InterruptedException e) { }
+
+				 firstFlipped.setVisibility(View.INVISIBLE);
+				 v.setVisibility(View.INVISIBLE);
+
+			} else {
 				flipFaceDown(firstFlipped);
 				flipFaceDown(v);
+				firstFlipped = null;
 			}
-			firstFlipped = null;
 		}
+
+		// unlock();
+		/*
+		 * try { Thread.sleep(2000); } catch (InterruptedException e) { }
+		 */
+
 	}
-	
-	private void flipFaceDown(TextView v){
-		v.setText("");
-		flip(v);
+
+	private void flipFaceDown(final TextView v) {
+		Animator.AnimatorListener animListener = new Animator.AnimatorListener() {
+
+			public void onAnimationCancel(Animator animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void onAnimationEnd(Animator animation) {
+				// TODO Auto-generated method stub
+				v.setText("");
+			}
+
+			public void onAnimationRepeat(Animator animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void onAnimationStart(Animator animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+		};
+
+		flip(v, animListener);
 	}
-	
-	private void flip(TextView v){
-		ObjectAnimator anim = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.flipping); 
+
+	// applies a flipping animation to a given view
+	private void flip(View v, Animator.AnimatorListener animListener) {
+		ObjectAnimator anim = (ObjectAnimator) AnimatorInflater.loadAnimator(
+				this, R.animator.flipping);
 		anim.setTarget(v);
 		anim.setDuration(1000);
+		anim.addListener(animListener);
 		anim.start();
 	}
-	
 
 	// TODO use real translations of the words that we got from the server
 	private class WordAndTranslationAdapter extends BaseAdapter {
@@ -142,6 +209,7 @@ public class Memory extends Activity implements Game {
 			return 0;
 		}
 
+		@SuppressWarnings("deprecation")
 		public View getView(int position, View convertView, ViewGroup parent) {
 			TextView textView;
 			if (convertView == null) { // if it's not recycled, initialize some
@@ -151,9 +219,7 @@ public class Memory extends Activity implements Game {
 				textView = (TextView) convertView;
 			}
 
-			//textView.setText(mThumbIds[position]);
 			textView.setTag(mThumbIds[position]);
-			//textView.setOnLongClickListener(new TranslationLongClickListener());
 			textView.setTextSize(16);
 			// textView.setSingleLine(false);
 			textView.setGravity(Gravity.CENTER_VERTICAL
@@ -162,27 +228,23 @@ public class Memory extends Activity implements Game {
 			// because it is introduced in API level 16
 			textView.setBackgroundDrawable(getResources().getDrawable(
 					R.drawable.memory_square_grid_item));
-			final int positon = position;
 			textView.setOnClickListener(new View.OnClickListener() {
-			
-				final int asd = positon;
 				public void onClick(View v) {
-					//Retrieve the values
-					Set<String> set = new HashSet<String>();
-					set.add("AM");
-					Log.v("AL BAKALIM>>> ",  (SP.getString("learning_language", "AM")).toString());
-					flipFaceUp((TextView)v);
+					// Retrieve the values
+
+					flipFaceUp((TextView) v);
 				}
 			});
-			
+
 			return textView;
 		}
 
 		// TODO change this to random mix of the translations of the words from
 		// server
 		// TODO possibly change this to an ArrayList
-		private String[] mThumbIds = { "besuchen", "rauchen", "Komputer",
-				"Schnee", "Test", "Test2", "Test", "Test2", "Test", "Test2", "Test", "Test2"};
+		private String[] mThumbIds = {"besuchen", "besuchen", "Komputer",
+				"Komputer", "Test", "Test2", "Test", "Test2", "Test", "Test2",
+				"Test", "Test2"};
 
 	}
 
