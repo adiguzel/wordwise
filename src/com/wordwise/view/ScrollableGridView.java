eaab26576b56afc8ed1b60098cc94857adda2250
Ugur Adiguzel <uguradgzl88@gmail.com>
2013-01-05 02:43:08 +0100

    LetterBox : added and used ScrollableGridView as a hack to put scroll view as container to grid views(by default not possible). scrolls are provided if layout does not fit into the screen

---
+++ src/com/wordwise/view/ScrollableGridView.java
@@ -0,0 +1,52 @@
+package com.wordwise.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.GridView;
+/**
+ * Used as a hack to put gridviews into scroll views which is by default not
+ * possible Stackoverflow discussion:
+ * http://stackoverflow.com/questions/9889255/how-to-add-multiple-gridviews-to-a-scrollview-in-android-java
+ **/
+public class ScrollableGridView extends GridView {
+	boolean expanded = true;
+
+	public ScrollableGridView(Context context) {
+		super(context);
+	}
+
+	public ScrollableGridView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public ScrollableGridView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public boolean isExpanded() {
+		return expanded;
+	}
+
+	@Override
+	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		// HACK! TAKE THAT ANDROID!
+		if (isExpanded()) {
+			// Calculate entire height by providing a very large height hint.
+			// But do not use the highest 2 bits of this integer; those are
+			// reserved for the MeasureSpec mode.
+			int expandSpec = MeasureSpec.makeMeasureSpec(
+					Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+			super.onMeasure(widthMeasureSpec, expandSpec);
+
+			ViewGroup.LayoutParams params = getLayoutParams();
+			params.height = getMeasuredHeight();
+		} else {
+			super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		}
+	}
+
+	public void setExpanded(boolean expanded) {
+		this.expanded = expanded;
+	}
+}
