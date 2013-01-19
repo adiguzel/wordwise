ece8508424e874d22f4251c73e85b12381601181
Ugur Adiguzel <uguradgzl88@gmail.com>
2013-01-19 17:14:11 +0100

    in the last commit forgot to mention the following: fixed ServerCommunication method names to match the server model such as rateWord became rateTranslation etc. Added Asyncronous rating save task to WordEvaluation. Not final.

--- src/com/wordwise/view/activity/game/WordEvaluation.java
+++ src/com/wordwise/view/activity/game/WordEvaluation.java
@@ -1,6 +1,13 @@
 package com.wordwise.view.activity.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
@@ -20,7 +27,7 @@ import com.wordwise.view.activity.WordwiseGameActivity;
 public class WordEvaluation extends WordwiseGameActivity implements Game {
 
 	private final String DIALOG_TITLE = "You can contribute! :)";
-	
+
 	private TextView wordToEvaluateText;
 	private RatingBar wordDifficultyRating;
 	private RatingBar wordQualityRating;
@@ -29,9 +36,10 @@ public class WordEvaluation extends WordwiseGameActivity implements Game {
 	// word to evaluate
 	private Word word;
 	private RESTfullServerCommunication serverCommunication;
+	Quality quality;
+	Difficulty difficulty;
 
 	private boolean qualityRated, difficultyRated = false;
-	
 
 	@Override
 	public void performOnCreate(Bundle savedInstanceState) {
@@ -39,48 +47,50 @@ public class WordEvaluation extends WordwiseGameActivity implements Game {
 		onGameInit();
 		onGameStart();
 	}
-	
+
 	private void initScreen() {
 		wordToEvaluateText = (TextView) findViewById(R.id.wordToEvaluate);
 		wordDifficultyRating = (RatingBar) findViewById(R.id.wordDifficultyRating);
 		wordQualityRating = (RatingBar) findViewById(R.id.wordQualityRating);
-		continueButton = (Button) findViewById(R.id.continueButton);	
-		submitTranslation = (Button) findViewById(R.id.submitTranslation); 
-		
-		wordDifficultyRating.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {	
-			public void onRatingChanged(RatingBar ratingBar, float rating,
-					boolean fromUser) {
-				difficultyRated = false;
-				if(rating > 0.0f){
-					difficultyRated = true;
-				}
-				checkSubmitCondition();
-			}
-		});
-		
-		wordQualityRating.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {	
-			public void onRatingChanged(RatingBar ratingBar, float rating,
-					boolean fromUser) {
-				qualityRated = false;
-				if(rating > 0.0f){
-					qualityRated = true;
-				}
-				checkSubmitCondition();
-			}
-		});
+		continueButton = (Button) findViewById(R.id.continueButton);
+		submitTranslation = (Button) findViewById(R.id.submitTranslation);
+
+		wordDifficultyRating
+				.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+					public void onRatingChanged(RatingBar ratingBar,
+							float rating, boolean fromUser) {
+						difficultyRated = false;
+						if (rating > 0.0f) {
+							difficultyRated = true;
+						}
+						checkSubmitCondition();
+					}
+				});
+
+		wordQualityRating
+				.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+					public void onRatingChanged(RatingBar ratingBar,
+							float rating, boolean fromUser) {
+						qualityRated = false;
+						if (rating > 0.0f) {
+							qualityRated = true;
+						}
+						checkSubmitCondition();
+					}
+				});
 	}
-	
-	private void checkSubmitCondition(){
-		if(qualityRated && difficultyRated){
+
+	private void checkSubmitCondition() {
+		if (qualityRated && difficultyRated) {
 			submitTranslation.setEnabled(true);
-		}
-		else
-			submitTranslation.setEnabled(false);		
+		} else
+			submitTranslation.setEnabled(false);
 	}
 
 	public Word retrieveWord() {
+		serverCommunication = new RESTfullServerCommunication();
 		// TODO get Word from the server
-		Word word  = new Word();
+		Word word = new Word();
 		word.setWord("test");
 		return word;
 	}
@@ -99,26 +109,31 @@ public class WordEvaluation extends WordwiseGameActivity implements Game {
 				this,
 				"quality : " + qualityRating + " difficulty: "
 						+ difficultyRating, Toast.LENGTH_LONG).show();
-		
-		Quality quality = new Quality();
+
+		quality = new Quality();
 		quality.setWord(word);
 		quality.setQuality(qualityRating);
-		
-		Difficulty difficulty = Difficulty.getByDifficulty(difficultyRating);
-		if(difficulty != null){
+
+		difficulty = Difficulty.getByDifficulty(difficultyRating);
+		if (difficulty != null) {
 			difficulty.setWord(word);
 		}
-		
-
-		// TODO Implement submitting evaluation and showing a toast if it was
-		// not successful
-		// TODO DTOWordRating rating = new DTOWordRating(difficultyRating,
-		// qualityRating, word);
-		// TODO wordServerComm.rateWord(rating);
-		
-		this.onGameEnd();
+		submitTranslation.setEnabled(false);
+		new WordRatingsSaveTask().execute();
 	}
-	
+
+	public void onSubmitSuccessful() {
+		onGameEnd();
+	}
+
+	public void onSubmitFailed() {
+		Toast.makeText(
+				this,
+				"Your evaluation could not be submitted to the server. Please try again.",
+				Toast.LENGTH_LONG).show();
+
+	}
+
 	public void onGameStart() {
 		// TODO Auto-generated method stub
 
@@ -149,4 +164,33 @@ public class WordEvaluation extends WordwiseGameActivity implements Game {
 		continueButton.setVisibility(View.VISIBLE);
 	}
 
+	public class WordRatingsSaveTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected void onPostExecute(Void result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			// TODO call either onSubmitSuccessful or onSubmitFailed
+			// depending on the connection result
+			// for now only
+			onGameEnd();
+			checkSubmitCondition();
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			Log.v("Word Ratings save task", "server conn started");
+
+			List<Difficulty> difficulties = new ArrayList<Difficulty>();
+			difficulties.add(difficulty);
+			serverCommunication.addWordDifficulties(difficulties);
+
+			List<Quality> qualities = new ArrayList<Quality>();
+			qualities.add(quality);
+			serverCommunication.addWordQualities(qualities);
+
+			Log.v("Word Ratings save task", "finished");
+	
+			return null;
+		}
+	}
 }
