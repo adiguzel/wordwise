c541f9720e8a27c2e76ca70fc60f79d4f4f551a3
Dragan Mileski <dragan.mileski@gmail.com>
2013-01-19 21:04:18 +0100

    several new methods implemented in TranslationEvaluation

--- src/com/wordwise/util/LanguageUtils.java
+++ src/com/wordwise/util/LanguageUtils.java
@@ -118,5 +118,6 @@ public class LanguageUtils {
 		context.getResources().updateConfiguration(config,
 				context.getResources().getDisplayMetrics());
 	}
+	
 
 }
--- src/com/wordwise/view/activity/game/TranslationEvaluation.java
+++ src/com/wordwise/view/activity/game/TranslationEvaluation.java
@@ -2,6 +2,7 @@ package com.wordwise.view.activity.game;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 import android.os.Bundle;
@@ -24,8 +25,11 @@ public class TranslationEvaluation extends WordwiseGameActivity {
 	private Set<Language> proficientLanguagesSet;
 	private List<Language> proficientLanguagesList = new ArrayList<Language>();
 
+	private List<Word> listOfEnglishWords;
+
 	private RESTfullServerCommunication serverCommunication;
 	private Word englishWord;
+	private Word translation;
 
 	private Button submitRating;
 	private Button continueButton;
@@ -33,7 +37,8 @@ public class TranslationEvaluation extends WordwiseGameActivity {
 	private TextView translationLanguageTitle;
 	private TextView translationToRate;
 	private RatingBar translationRating;
-	
+
+	private Language englishLanugage = new Language("English", "en");
 	private Language languageOfTranslation;
 
 	@Override
@@ -52,9 +57,21 @@ public class TranslationEvaluation extends WordwiseGameActivity {
 		translationLanguageTitle = (TextView) findViewById(R.id.translationTitle);
 		translationToRate = (TextView) findViewById(R.id.translationToRate);
 		translationRating = (RatingBar) findViewById(R.id.translationRatingBar);
+
+		configuration = Configuration.getInstance(this);
+		proficientLanguagesSet = this.configuration.getProficientLanguages();
+		proficientLanguagesList = LanguageUtils
+				.getProficientLanguages(this.proficientLanguagesSet);
 		
 		languageOfTranslation = chooseRandomProficientLanguage();
-		submitRating.setEnabled(true);
+		
+		englishWord = this.retrieveRandomEnglishWord();
+//		translation = 
+
+		this.setTextViews();
+
+		submitRating.setVisibility(View.VISIBLE);
+		continueButton.setVisibility(View.INVISIBLE);
 	}
 
 	public void onGameStart() {
@@ -74,33 +91,50 @@ public class TranslationEvaluation extends WordwiseGameActivity {
 
 	public void onGameEnd() {
 		// TODO Auto-generated method stub
-		submitRating.setEnabled(false);
-		continueButton.setEnabled(true);
+		submitRating.setVisibility(View.INVISIBLE);
+		continueButton.setVisibility(View.VISIBLE);
 	}
-	
-	private void submitRating(View v) {
-		
+
+	private void setTextViews() {
+		this.wordInEnglish.setText(this.englishWord.getWord());
+		this.translationToRate.setText(this.translation.getWord());
+		this.translationLanguageTitle.setText(this.translationLanguageTitle.getText() + " " + this.languageOfTranslation.getLanguage());
+	}
+
+	private Word retrieveRandomEnglishWord() {
+		Word randomEnglishWord = new Word();
+		this.serverCommunication = new RESTfullServerCommunication();
+		//maybe should be this.serverCommunication.listWords(englishLanugage); should be changed by adding difficulty
+		this.listOfEnglishWords = this.serverCommunication.listWords(englishLanugage);
+		int randomPosition = this.randomNumber(listOfEnglishWords.size());
+		randomEnglishWord = this.listOfEnglishWords.get(randomPosition);
+		return randomEnglishWord;
+	}
+
+	private int randomNumber(int max) {
+		Random randomGenerator = new Random();
+		int randomNumber = randomGenerator.nextInt(max);
+		return randomNumber;
 	}
 
 	private Language chooseRandomProficientLanguage() {
-
-		configuration = Configuration.getInstance(this);
-		proficientLanguagesSet = configuration.getProficientLanguages();
-		proficientLanguagesList = LanguageUtils
-				.getProficientLanguages(proficientLanguagesSet);
-
 		// removing English since this is the language from which the words are
 		// being translated
-		if (proficientLanguagesList
-				.contains(LanguageUtils.getByName("English"))) {
-			proficientLanguagesList.remove(proficientLanguagesList
+		if (this.proficientLanguagesList.contains(LanguageUtils
+				.getByName("English"))) {
+			this.proficientLanguagesList.remove(this.proficientLanguagesList
 					.indexOf(LanguageUtils.getByName("English")));
 		}
 
 		Language randomLanguage = LanguageUtils
-				.getRandomProficientLanguage(proficientLanguagesList);
+				.getRandomProficientLanguage(this.proficientLanguagesList);
 
 		return randomLanguage;
+	}
+
+	private void submitRating(View v) {
+		//send the rating
+		this.onGameEnd();
 	}
 
 }
