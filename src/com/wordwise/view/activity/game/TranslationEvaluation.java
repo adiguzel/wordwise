62943cca2117a37e018e5ebfc6bc70cd4e830a9d
Dragan Mileski <dragan.mileski@gmail.com>
2013-01-19 20:32:03 +0100

    Some progrès in TraslationEvaluation

--- src/com/wordwise/view/activity/game/TranslationEvaluation.java
+++ src/com/wordwise/view/activity/game/TranslationEvaluation.java
@@ -1,15 +1,40 @@
 package com.wordwise.view.activity.game;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.wordwise.R;
-import com.wordwise.util.WordwiseUtils;
+import com.wordwise.client.RESTfullServerCommunication;
+import com.wordwise.model.Configuration;
+import com.wordwise.server.model.Language;
+import com.wordwise.server.model.Word;
+import com.wordwise.util.LanguageUtils;
 import com.wordwise.view.activity.WordwiseGameActivity;
 
 public class TranslationEvaluation extends WordwiseGameActivity {
+
+	private Configuration configuration;
+	private Set<Language> proficientLanguagesSet;
+	private List<Language> proficientLanguagesList = new ArrayList<Language>();
+
+	private RESTfullServerCommunication serverCommunication;
+	private Word englishWord;
+
+	private Button submitRating;
+	private Button continueButton;
+	private TextView wordInEnglish;
+	private TextView translationLanguageTitle;
+	private TextView translationToRate;
+	private RatingBar translationRating;
 	
-	private final String DIALOG_MESSAGE = "In this screen you will be asked to do a small contribution for this application. Please rate a word translation on your preffered language...";
-	private final String DIALOG_TITLE = "You can contribute! :)";
+	private Language languageOfTranslation;
 
 	@Override
 	public void performOnCreate(Bundle savedInstanceState) {
@@ -17,33 +42,65 @@ public class TranslationEvaluation extends WordwiseGameActivity {
 		setContentView(R.layout.rate_translation);
 		onGameInit();
 		onGameStart();
-		WordwiseUtils.infoDialogOnStart(DIALOG_TITLE,DIALOG_MESSAGE,this);
+
 	}
 
 	public void onGameInit() {
-		// TODO Auto-generated method stub
+		submitRating = (Button) findViewById(R.id.submitRatingButton);
+		continueButton = (Button) findViewById(R.id.continueButton);
+		wordInEnglish = (TextView) findViewById(R.id.wordInEnglish);
+		translationLanguageTitle = (TextView) findViewById(R.id.translationTitle);
+		translationToRate = (TextView) findViewById(R.id.translationToRate);
+		translationRating = (RatingBar) findViewById(R.id.translationRatingBar);
 		
+		languageOfTranslation = chooseRandomProficientLanguage();
+		submitRating.setEnabled(true);
 	}
 
 	public void onGameStart() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onGameStop() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onGamePause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onGameEnd() {
 		// TODO Auto-generated method stub
+		submitRating.setEnabled(false);
+		continueButton.setEnabled(true);
+	}
+	
+	private void submitRating(View v) {
 		
 	}
 
-	
+	private Language chooseRandomProficientLanguage() {
+
+		configuration = Configuration.getInstance(this);
+		proficientLanguagesSet = configuration.getProficientLanguages();
+		proficientLanguagesList = LanguageUtils
+				.getProficientLanguages(proficientLanguagesSet);
+
+		// removing English since this is the language from which the words are
+		// being translated
+		if (proficientLanguagesList
+				.contains(LanguageUtils.getByName("English"))) {
+			proficientLanguagesList.remove(proficientLanguagesList
+					.indexOf(LanguageUtils.getByName("English")));
+		}
+
+		Language randomLanguage = LanguageUtils
+				.getRandomProficientLanguage(proficientLanguagesList);
+
+		return randomLanguage;
+	}
+
 }
