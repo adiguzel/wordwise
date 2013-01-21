f3cc0c71df93b548a4346de499d862174016870a
Giovanni Maia <giovanni.maia@gmail.com>
2013-01-21 13:36:06 +0100

    implementation of the "new" server methods.

--- src/com/wordwise/client/RESTfullServerCommunication.java
+++ src/com/wordwise/client/RESTfullServerCommunication.java
@@ -10,45 +10,85 @@ import com.wordwise.server.model.Language;
 import com.wordwise.server.model.Quality;
 import com.wordwise.server.model.Rate;
 import com.wordwise.server.model.Translation;
-import com.wordwise.server.model.Word;
 import com.wordwise.server.model.parameter.ListTranslationParameters;
-import com.wordwise.server.model.parameter.ListWordParameters;
+import com.wordwise.server.resource.DifficultyResource;
 import com.wordwise.server.resource.QualityResource;
 import com.wordwise.server.resource.RateResource;
 import com.wordwise.server.resource.TranslationResource;
-import com.wordwise.server.resource.WordResource;
 
 public class RESTfullServerCommunication implements ServerCommunication
 {
 	private static final String BASE_CLIENT_URL = "http://192.168.112.1:8080/WordWiseServer/";
 	
-	private static final WordResource wordResource = new ClientResource(BASE_CLIENT_URL+WordResource.RESOURCE_NAME).wrap(WordResource.class);
-	private static final TranslationResource translationResource = new ClientResource(BASE_CLIENT_URL+TranslationResource.RESOURCE_NAME).wrap(TranslationResource.class);
-	private static final RateResource rateResource = new ClientResource(BASE_CLIENT_URL+RateResource.RESOURCE_NAME).wrap(RateResource.class);
-	private static final QualityResource qualityResource = new ClientResource(BASE_CLIENT_URL+QualityResource.RESOURCE_NAME).wrap(QualityResource.class);
+	private static final TranslationResource translationResource = getTranslationResource();
+	private static final RateResource rateResource = getRateResource();
+	private static final QualityResource qualityResource = getQualityResource();
+	private static final DifficultyResource difficultyResource = getDifficultyResource();
 	
+	private static TranslationResource getTranslationResource()
+	{
+		ClientResource clientResource = new ClientResource(BASE_CLIENT_URL+TranslationResource.RESOURCE_NAME);
+		
+		clientResource.setRetryOnError(false);
+		clientResource.getContext().getParameters().add("socketTimeout", "1000");
+		
+		return clientResource.wrap(TranslationResource.class);
+	}
 	
-	public boolean addTranslation(Translation translation) {
-		// TODO Auto-generated method stub
-		return false;
+	private static RateResource getRateResource()
+	{
+		ClientResource clientResource = new ClientResource(BASE_CLIENT_URL+RateResource.RESOURCE_NAME);
+		
+		clientResource.setRetryOnError(false);
+		clientResource.getContext().getParameters().add("socketTimeout", "1000");
+		
+		return clientResource.wrap(RateResource.class);
 	}
-	public boolean rateTranslation(Rate rating) {
-		// TODO Auto-generated method stub
-		return false;
+	
+	private static QualityResource getQualityResource()
+	{
+		ClientResource clientResource = new ClientResource(BASE_CLIENT_URL+QualityResource.RESOURCE_NAME);
+		
+		clientResource.setRetryOnError(false);
+		clientResource.getContext().getParameters().add("socketTimeout", "1000");
+
+		return clientResource.wrap(QualityResource.class);
 	}
-	public boolean addWordQualitiy(Quality quality) {
-		// TODO Auto-generated method stub
-		return false;
+	
+	private static DifficultyResource getDifficultyResource()
+	{
+		ClientResource clientResource = new ClientResource(BASE_CLIENT_URL+DifficultyResource.RESOURCE_NAME);
+		
+		clientResource.setRetryOnError(false);
+		clientResource.getContext().getParameters().add("socketTimeout", "1000");
+
+		return clientResource.wrap(DifficultyResource.class);
 	}
-	public boolean addWordDifficulty(Difficulty difficulty) {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public boolean addTranslation(Translation translation)
+	{
+		translationResource.add(translation);
+		return true;
 	}
-	public List<Translation> listTranslations(Language lang,
-			Difficulty difficulty, int number,
-			List<Translation> translationsAlreadyUsed) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public boolean rateTranslation(Rate rating)
+	{
+		rateResource.add(rating);
+		return true;
+	}
+	public boolean addWordQualitiy(Quality quality)
+	{
+		qualityResource.add(quality);
+		return true;
+	}
+	public boolean addWordDifficulty(Difficulty difficulty)
+	{
+		difficultyResource.add(difficulty);
+		return true;
+	}
+	public List<Translation> listTranslations(Language language, Difficulty difficulty, int numberOfTranslations,	List<Translation> translationsAlreadyUsed)
+	{
+		return translationResource.list(new ListTranslationParameters(language, difficulty, numberOfTranslations, translationsAlreadyUsed));
 	}
 
 }
