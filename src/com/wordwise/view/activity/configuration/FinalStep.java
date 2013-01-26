fa7b66e66810e60fe9f456a38bf8abc07234d09e
Ugur Adiguzel <uguradgzl88@gmail.com>
2013-01-26 21:27:16 +0100

    Template final and initial step activities are added for conf.

---
+++ src/com/wordwise/view/activity/configuration/FinalStep.java
@@ -0,0 +1,21 @@
+package com.wordwise.view.activity.configuration;
+
+import android.view.View;
+
+import com.wordwise.view.activity.ConfigurationStep;
+
+public class FinalStep extends ConfigurationStep{
+
+	@Override
+	public boolean isFinished() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	protected void performOnCreate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/com/wordwise/view/activity/configuration/InitialStep.java
@@ -0,0 +1,20 @@
+package com.wordwise.view.activity.configuration;
+
+import android.view.View;
+
+import com.wordwise.view.activity.ConfigurationStep;
+
+public class InitialStep extends ConfigurationStep{
+
+	@Override
+	public boolean isFinished() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	protected void performOnCreate() {
+		// TODO Auto-generated method stub
+		
+	}
+}
