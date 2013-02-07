41397478f48316f5c9fdc6cbd23763a6d0d004f8
Ugur Adiguzel <uguradgzl88@gmail.com>
2013-02-07 01:02:28 +0100

    Replaced intro animation text with new cool wordwise logo. Thanks to our designer Lukasz Kwiatkowski

--- src/com/wordwise/WordwiseIntro.java
+++ src/com/wordwise/WordwiseIntro.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.tekle.oss.android.animation.AnimationFactory;
 import com.wordwise.controller.ConfigurationProcess;
@@ -12,7 +12,7 @@ import com.wordwise.model.Configuration;
 import com.wordwise.util.LanguageUtils;
 
 public class WordwiseIntro extends Activity {
-	private TextView introText;
+	private ImageView wordwiseLogo;
 	private Configuration configuration;
 	
 	@Override
@@ -21,13 +21,12 @@ public class WordwiseIntro extends Activity {
 		getActionBar().hide();
 		setContentView(R.layout.wordwise_intro);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		introText = (TextView) findViewById(R.id.wordwiseIntroText);
-		introText.startAnimation(AnimationFactory.inFromLeftAnimation(1000,
+		wordwiseLogo = (ImageView) findViewById(R.id.wordwiseLogo);
+		wordwiseLogo.startAnimation(AnimationFactory.inFromLeftAnimation(1000,
 				null));
-		introText.postDelayed(new Runnable() {
+		wordwiseLogo.postDelayed(new Runnable() {
 
 			public void run() {
-				// TODO Auto-generated method stub
 				LanguageUtils.init(getResources());
 				configuration = Configuration
 						.getInstance(getApplicationContext());
